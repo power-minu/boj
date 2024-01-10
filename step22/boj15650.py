2dd1432b5926537a097ec4050d0891c0488c31cc
power-minu <mw0677@naver.com>
2024-01-10 10:55:47 +0900

    feat: 22단계 15650번

---
+++ step22/boj15650.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline()
+
+N, M = map(int, input.split())
+s = []
+
+def backtracking(start):
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(start, N+1):
+        if i not in s:
+            s.append(i)
+            backtracking(i+1)
+            s.pop()
+
+backtracking(1)
